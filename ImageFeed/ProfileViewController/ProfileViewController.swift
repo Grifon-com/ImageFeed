af5d6c6ed00dcd16a8b9dad5cc2471d5b266206f
Grifon <mashukgrisha@mail.ru>
2023-06-15 17:00:47 +0300

    fef

--- ImageFeed/ProfileViewController/ProfileViewController.swift
+++ ImageFeed/ProfileViewController/ProfileViewController.swift
@@ -12,12 +12,19 @@ class ProfileViewController: UIViewController {
     private var avatarImageView: UIImageView?
     private var nameLabel: UILabel?
     private var loginLable: UILabel?
+    private var descriptionLable: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .ypBlack
         setupAvatarImageView()
+        setupNameLabel()
+        setupLoginLable()
+        setupDescriptionLable()
+        setupLogautButton()
     }
+    @objc
+    func didTapLogoutButton() {}
 }
 
 extension ProfileViewController {
@@ -45,7 +52,65 @@ extension ProfileViewController {
     func setupNameLabel() {
         let nameLabel = UILabel()
         nameLabel.text = "Екатерина новикова"
-        nameLabel.tintColor = .ypWhite
-        nameLabel.font = UIFont.init(name: <#T##String#>, size: <#T##CGFloat#>)
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = UIFont.init(name: "HelveticaNeue-Bold", size: 23)
+        setupAutoresizingMaskBackgroundColor(andAdd: nameLabel)
+        self.nameLabel = nameLabel
+        
+        guard let avatarImageView = avatarImageView else { return }
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    func setupLoginLable() {
+        let loginLable = UILabel()
+        loginLable.text = "@ekaterina_nov"
+        loginLable.textColor = .ypGrey
+        loginLable.font = UIFont.systemFont(ofSize: 13)
+        setupAutoresizingMaskBackgroundColor(andAdd: loginLable)
+        self.loginLable = loginLable
+        
+        guard let nameLabel = nameLabel else {
+            return }
+        
+        NSLayoutConstraint.activate([
+            loginLable.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    func setupDescriptionLable() {
+        let descriptionLable = UILabel()
+        descriptionLable.text = "Hello, world!"
+        descriptionLable.textColor = .ypWhite
+        descriptionLable.font = UIFont.systemFont(ofSize: 13)
+        setupAutoresizingMaskBackgroundColor(andAdd: descriptionLable)
+        self.descriptionLable = descriptionLable
+        
+        guard let loginLable = loginLable else { return }
+        
+        NSLayoutConstraint.activate([
+            descriptionLable.topAnchor.constraint(equalTo: loginLable.bottomAnchor, constant: 8),
+            descriptionLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    func setupLogautButton() {
+        let imageButton = UIImage(named: "Exit") ?? UIImage()
+        let logautButton = UIButton.systemButton(with: imageButton, target: self, action: #selector(self.didTapLogoutButton))
+        logautButton.tintColor = .ypRed
+        setupAutoresizingMaskBackgroundColor(andAdd: logautButton)
+        
+        guard let avatarImageView = avatarImageView else { return }
+        
+        NSLayoutConstraint.activate([
+            logautButton.heightAnchor.constraint(equalToConstant: 44),
+            logautButton.widthAnchor.constraint(equalToConstant: 44),
+            logautButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            logautButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
