9f4a69e5b1fe5d0ee615606c7dcdd583d3535ab1
Grifon <mashukgrisha@mail.ru>
2023-07-01 14:56:42 +0300

    d

---
+++ ImageFeed/TestViewController/GreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GreenViewController.swift
+//  ImageFeed
+//
+//  Created by Марина Машук on 1.07.23.
+//
+
+
+import UIKit
+
+final class GreenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
---
+++ ImageFeed/TestViewController/RedViewController.swift
@@ -0,0 +1,46 @@
+//
+//  RedViewController.swift
+//  ImageFeed
+//
+//  Created by Марина Машук on 1.07.23.
+//
+
+import UIKit
+
+final class RedViewController: UIViewController {
+    private var segue: UIStoryboardSegue?
+    
+    private let buttonView: UIButton = {
+        let size = CGSize.init(width: 100, height: 50)
+        let origin = CGPoint(x: 100, y: 100)
+        let frame = CGRect(origin: origin, size: size)
+        let buttonView = UIButton(frame: frame)
+        buttonView.titleLabel?.textColor = .ypBackground
+        buttonView.setTitle("Переход", for: .normal)
+        
+        return buttonView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .ypRed
+        view.addSubview(buttonView)
+        let storybord = UIStoryboard(name: "Main", bundle: .main)
+        let greenVc = storybord.instantiateViewController(identifier: "greenViewController")
+        let redVc = storybord.instantiateViewController(withIdentifier: "redViewController")
+        
+        segue = UIStoryboardSegue(identifier: "ShowGreenViewController", source: greenVc, destination: redVc)
+        
+        buttonView.addTarget(self, action: #selector(didShowGreenVc), for: .touchUpInside)
+        
+    }
+    
+    
+    @objc private func didShowGreenVc(_ sender: UIButton) {
+        if self.segue?.identifier == "ShowGreenViewController" {
+            guard let vc = navigationController?.viewControllers.first(where: {$0.}) else { return }
+            present(vc, animated: true)
+        }
+    }
+}
+
