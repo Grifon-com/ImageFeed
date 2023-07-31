e659f97485cd10dfdd262f57e857a392b7489b2e
Grifon-com <128435394+Grifon-com@users.noreply.github.com>
2023-07-31 13:04:45 +0300

    Добавил класс для работы с датой

---
+++ ImageFeed/Helpers/ResumeDate.swift
@@ -0,0 +1,37 @@
+//
+//  ResumeDate.swift
+//  ImageFeed
+//
+//  Created by Марина Машук on 31.07.23.
+//
+
+import Foundation
+
+final class ResumeDate {
+    let shared = ResumeDate()
+
+    private lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter
+    }()
+
+    private lazy var iSO8601DateFormatter: ISO8601DateFormatter = {
+        let iSO8601DateFormatter = ISO8601DateFormatter()
+
+        return iSO8601DateFormatter
+    }()
+
+    func setupModelDate(createAt: String?) throws -> Date {
+        guard let createAt = createAt,
+              let date = iSO8601DateFormatter.date(from: createAt) else { throw  ErrorDateFormat.dateError}
+        return date
+    }
+
+    func setupUIDateString(date: Date) -> String {
+        let date = dateFormatter.string(from: date)
+
+        return date
+    }
+}
