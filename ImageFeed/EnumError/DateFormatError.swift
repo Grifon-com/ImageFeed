ce96527e70cfe8f76ea7d87d678f9108ce3ec78e
Grifon-com <128435394+Grifon-com@users.noreply.github.com>
2023-07-31 13:02:02 +0300

    Добавил клас Clear для очистки все куки из хранилища и удаления токена

---
+++ ImageFeed/EnumError/DateFormatError.swift
@@ -0,0 +1,12 @@
+//
+//  DateFormatError.swift
+//  ImageFeed
+//
+//  Created by Марина Машук on 31.07.23.
+//
+
+import Foundation
+
+enum ErrorDateFormat: Error {
+    case dateError
+}
---
+++ ImageFeed/Services/Clear.swift
@@ -0,0 +1,35 @@
+//
+//  Clear.swift
+//  ImageFeed
+//
+//  Created by Марина Машук on 31.07.23.
+//
+
+import Foundation
+import WebKit
+
+protocol CleanProtocol {
+    func cleanCookies()
+    func cleanToken()
+}
+
+final class Clean: CleanProtocol {
+    private let tokenStorage = OAuth2TokenKeychainStorage()
+    
+    func cleanCookies() {
+        // Очищаем все куки из хранилища.
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        // Запрашиваем все данные из локального хранилища.
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { record in
+            // Массив полученных записей удаляем из хранилища.
+            record.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
+    }
+    
+    func cleanToken() {
+        // удаляем токен из KeyChain
+        tokenStorage.removeSuccessful()
+    }
+}
